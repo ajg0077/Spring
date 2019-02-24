8d178015ecfaf71a10fbe3808dbd266c2b375282
Antony <ajg0077@gmail.com>
2019-02-24 21:41:21 +0530

    Destroy method invoked with Abstract ApplicationCOntext and registerShutdownHook()

--- EclipseWorkSpace/SpringApp3/src/employee13_Initialize_DisposibleBean/MainDriver.java
+++ EclipseWorkSpace/SpringApp3/src/employee13_Initialize_DisposibleBean/MainDriver.java
@@ -1,14 +1,16 @@
 package employee13_Initialize_DisposibleBean;
 
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.AbstractApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class MainDriver {
 	public static void main(String[] args) {
 		
-		ApplicationContext context = new ClassPathXmlApplicationContext("Spring5.xml");
+		AbstractApplicationContext context = new ClassPathXmlApplicationContext("Spring5.xml");
 		Manager manager = context.getBean("aliasName", Manager.class);
 		manager.address();
 		
+		context.registerShutdownHook();
 	}
 }
