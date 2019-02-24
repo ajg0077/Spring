d5b0cf39c5432c2a4e35a4dc52153036c9abb9f3
Antony <ajg0077@gmail.com>
2019-02-24 17:32:54 +0530

    List injection

--- EclipseWorkSpace/SpringApp3/src/employee10_CollectionInjection2/Manager.java
+++ EclipseWorkSpace/SpringApp3/src/employee10_CollectionInjection2/Manager.java
@@ -1,40 +1,23 @@
 package employee10_CollectionInjection2;
 
+import java.util.List;
+
 public class Manager implements EmployeeAddress {
+	private List<Address> address;
 
-	private Address addressa;
-	private Address addressb;
-	private Address addressc;
-
-	public Address getAddressa() {
-		return addressa;
+	public List<Address> getAddress() {
+		return address;
 	}
 
-	public void setAddressa(Address addressa) {
-		this.addressa = addressa;
-	}
-
-	public Address getAddressb() {
-		return addressb;
-	}
-
-	public void setAddressb(Address addressb) {
-		this.addressb = addressb;
-	}
-
-	public Address getAddressc() {
-		return addressc;
-	}
-
-	public void setAddressc(Address addressc) {
-		this.addressc = addressc;
+	public void setAddress(List<Address> address) {
+		this.address = address;
 	}
 
 	@Override
 	public void address() {
-		System.out.println("Address A =" + getAddressa().getCity() + " " + getAddressa().gethNo());
-		System.out.println("Address B =" + getAddressb().getCity() + " " + getAddressb().gethNo());
-		System.out.println("Address C =" + getAddressc().getCity() + " " + getAddressc().gethNo());
+		for(Address a: address) {
+			System.out.println(a.getCity()+ " " + a.gethNo());
+		}
 	}
 
 }
